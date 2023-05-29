ddf521e66cd9136d03d5048158799beb66e29840
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2023-05-29 17:40:25 +0530

    chore: add necessary deployment packages

--- src/contracts/hardhat.config.js
+++ src/contracts/hardhat.config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
-require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
 
 module.exports = {
   defaultNetwork: "matic",
@@ -11,6 +11,11 @@ module.exports = {
       url: "https://rpc-mumbai.maticvigil.com",
       accounts: [process.env.PRIVATE_KEY]
     },
+    gnosis: {
+      url: "https://rpc.gnosischain.com/",
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 1100000000
+    }
   },
   solidity: {
     version: "0.8.13",
