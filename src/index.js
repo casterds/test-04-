d2c9e2fef71e9f20ddc2cd6bc426d73911ec6b91
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2023-05-29 22:39:36 +0530

    chore: integrated Banana wallet

--- src/index.js
+++ src/index.js
@@ -1,51 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import '@rainbow-me/rainbowkit/styles.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "@rainbow-me/rainbowkit/styles.css";
+
 import {
-  getDefaultWallets,
+  wagmiConfig,
   RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import {
-  chain,
-  configureChains,
-  createClient,
   WagmiConfig,
-} from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
-import { ChakraProvider } from '@chakra-ui/react';
+  chains,
+  polygonMumbai,
+} from "./configureRainbowkit";
+import { darkTheme } from "@rainbow-me/rainbowkit";
+import { ChakraProvider } from "@chakra-ui/react";
 
-const { chains, provider } = configureChains(
-  [chain.polygonMumbai, chain.ropsten],
-  [
-    alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }),
-    publicProvider()
-  ]
-);
-
-const { connectors } = getDefaultWallets({
-  appName: 'My RainbowKit App',
-  chains
-});
-
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider
-})
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
+    <WagmiConfig client={wagmiConfig}>
+      <RainbowKitProvider
+        chains={chains}
+        initialChain={polygonMumbai}
+        theme={darkTheme()}
+      >
         <ChakraProvider>
           <App />
         </ChakraProvider>
-    </RainbowKitProvider>
+      </RainbowKitProvider>
     </WagmiConfig>
   </React.StrictMode>
 );
