935e958142bdc92bc8b5ffe953ab5d21d497ea02
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2023-05-31 11:14:21 +0530

    chore: updated github url

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return ( 
         <div className='w-screen h-fit p-4 flex flex-row bg-hero items-center justify-center' >
-            <label className='text-white font-normal text-lg' ><a target='_blank' href='https://github.com/Arch0125/salariedxyz' >Checkout on GitHub</a></label>
+            <label className='text-white font-normal text-lg' ><a target='_blank' href='https://github.com/codebuster22/test-04-' >Checkout on GitHub</a></label>
         </div>
      );
 }
